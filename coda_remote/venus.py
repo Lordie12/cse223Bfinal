1edc55610d20e159adfe4325568383cb1bd0266c
Kaushik Kalyan <Lanfear@Kaushiks-MacBook-Pro.local>
2014-05-29 20:15:55 -0700

    Few modifications

--- coda_remote/venus.py
+++ coda_remote/venus.py
@@ -3,88 +3,21 @@
 import os
 import hashlib
 import collections
+import rpyc
 
 def hash(filename):
 	return hashlib.md5(filename).hexdigest()
-
-class Cache:
-	"""Cache, has an active list of cached files
-	and a bitmap indicating dirty files."""
-	def __init__(self):
-		self.filecache = []
-		self.bitmap = {}
-		self.log = []
-		self.FD = {}
-
-	def exists(self, filename):
-		"""See if file exists in cache"""
-		direntry, filename = os.path.split(filename)
-		#if directory does not exist
-		if not os.path.exists(direntry + '/'):
-			return False
-		else:
-			#if file does not exist
-			if filename not in os.listdir(direntry):
-				return False
-		return True
-
-	def get_FD(self, filename):
-		return self.FD[hash(filename)]
-
-	def cache_add(self, filename, mode):
-		"""Bring file into cache from remote server
-		if not found in local cache"""
-		self.filecache.append(filename)
-		self.bitmap[hash(filename)] = 1
-		ofname = filename
-
-		#code to bring file from server, if this fails
-		#create local copy and send it to server
-		direntry, filename = os.path.split(filename)
-		try:
-			#Creating new directory in cache
-			os.mkdir(direntry + '/')
-			#todo: create directories in server too
-		except OSError:
-			#directory already exists
-			pass
-
-		self.FD[hash(ofname)] = open(ofname, mode)
-		#todo: create file in server too 
-
-	def cache_read(self, filename):
-		self.FD[hash(filename)] = open(filename, 'r')
-		data = self.FD[hash(filename)].read()
-		self.FD[hash(filename)].close()
-		return data
-
-	def cache_delete(self, filename):
-		try:
-			os.remove(filename)
-		except OSError:
-			print 'File not found'
-			return
-		print 'File deleted'
-		self.log.append("Timestamp: " + filename + "Operation: deleted")
-	
-	def write_logs(self, filename, data):
-		self.log.append("Timestamp: " + filename + "Operation: Write " + "Data: " + data)
-	def print_logs(self):
-		return self.log
 
 class Venus:
 	"""Local Venus cache manager, checks 
 	cache for file, returns local FD if file exists,
 	query server manager if no local copy"""
-	def __init__(self, master):
-		self.master = master
-		self.cache = Cache()
+	def __init__(self):
+		self.conn = rpyc.connect('localhost', 2222)
 
-	def print_logs(self):
-		return self.cache.print_logs()
-
-	def write_logs(self, filename, data):
-		self.cache.write_logs(filename, data)
+	def create(self, path, mode):
+		#If remote file also does not exist, then create
+		return self.conn.root.create(path)
 
 	def write(self, filename,  data): # filename is full namespace path
 		"""Full namespace with path, if present in local cache, return
@@ -98,34 +31,3 @@ class Venus:
 
 		self.write_file(FD, data)
 		self.write_logs(filename, data)
-
-    	def write_file(self, FD, data):
-		FD.write(data)
-		FD.close()
-
-    	def write_append(self, filename, data):
-        	if not self.exists(filename):
-            		raise Exception("append error, file does not exist: " \
-                 	+ filename)
-		self.cache_add(filename, 'a')	
-		FD = self.get_FD(filename)
-		FD.write(data)
-		FD.close()
-
-	def cache_add(self, filename, mode):
-		return self.cache.cache_add(filename, mode)
-
-    	def exists(self, filename):
-		return self.cache.exists(filename)
-
-	def get_FD(self, filename):
-		return self.cache.get_FD(filename)
-
-    	def read(self, filename):
-        	if not self.exists(filename):
-            		raise Exception("read error, file does not exist: " \
-                	+ filename)
-        	return self.cache.cache_read(filename)
-
-	def delete(self, filename):
-		self.cache.cache_delete(filename)
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from stat import S_IFDIR, S_IFLNK, S_IFREG
 from sys import argv, exit
 from time import time
 from fuse import FUSE, FuseOSError, Operations
-import coda_remote.venus
-import json, pickle, os
+from coda_remote.venus import Venus
+from pyftpdlib.log import logger
+from pyftpdlib.ioloop import _config_logging
+import json, pickle, os, logging, sys, rpyc, threading
+from thread import start_new_thread
+from rpyc.utils.server import ThreadedServer
 
 if not hasattr(__builtins__, 'bytes'):
 	bytes = str
@@ -18,8 +22,7 @@ class FS(Operations):
 	self.data stores filedata, self.fd is the filedescriptor"""
 
    	def __init__(self):
-		"""Database to store file information"""
-        	
+		_config_logging()
 		self.files = {}
         	self.data = defaultdict(bytes)
         	self.fd = 0
@@ -28,51 +31,100 @@ class FS(Operations):
         	now = time()
 		st_mode =  (S_IFDIR | 0755) 
 		st_mtime = st_atime = st_ctime = now
-		st_nlink = int(2)
+		st_nlink = 2
 		f_dir = list()
-        	self.files['/'] = dict(st_mode=(S_IFDIR | 0755), st_ctime = now,
+
+		#If root metadata exists, then load it in-memory 
+		if os.path.isfile('/cache/root.dmeta'):
+			logger.info('Root metadata exists, loading into memory')
+			self.files = pickle.load(open('/cache/root.dmeta', 'r'))
+		else:
+		#Recreate new root directory
+        		self.files['/'] = dict(st_mode=(S_IFDIR | 0755), st_ctime = now,
                                st_mtime = now, st_atime = now, st_nlink = 2, f_dir = list())
 
 		if not os.path.exists('/cache'):
 			os.mkdir('/cache')
 
     	def chmod(self, path, mode):
+		logger.info("Changing permissions of file %s into %s" % (path, mode))
         	self.files[path]['st_mode'] &= 0770000
         	self.files[path]['st_mode'] |= mode
+		f = open('/cache' + path + '.meta', 'w')
+		f.write(self.files[path])
+		f.close()
         	return 0
 
     	def chown(self, path, uid, gid):
+		logger.info("Changing permissions of file %s into %s" % (path, mode))
         	self.files[path]['st_uid'] = uid
         	self.files[path]['st_gid'] = gid
+		f = open('/cache' + path + '.meta', 'w')
+		f.write(self.files[path])
+		f.close()
 
     	def create(self, path, mode):
 		"""Create a file at the specified path with the specified mode,
 		mode is by default S_IFREG which is the linux equivalent
-		of a regular file ORed with the mode specified"""
+		of a regular file ORed with the mode specified
+		Creating a new file does not create issues at the local cache
+		if the file exists but may cause issues if local cache is not 
+		the most updated copy because Venus will have to fetch entries
+		into cache before issuing a create request"""
 
-        	self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1,
-                                st_size=0, st_ctime=time(), st_mtime=time(),
-                                st_atime=time())
+		logger.info("Creating a new file %s with mode %s" % (path, mode))
+		res = self.venus.create(path, mode)
+	
+		if res['Status'] == False:
+			#File does not exist
+			self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1,
+					st_size=0, st_ctime=time(), st_mtime=time(),
+					st_atime=time())
+			
+			#Not root directory
+			if len(path.split('/')) != 2:
+				self.files[path[:path.rfind('/')]]['f_dir'].append(path)
+				#Store parentdir metadata
+				pickle.dump(self.files[path[:path.rfind('/')]], open('/cache' + path[:path.rfind('/')] + '.dmeta', 'w+')) 
+			else:
+			#Root directory
+				self.files['/']['f_dir'].append(path)
+				pickle.dump(self.files, open('/cache/root.dmeta', 'w+'))
+
+			#Store newfile meta
+			pickle.dump(self.files[path], open('/cache' + path + '.meta', 'w+'))
+			f = open('/cache' + path, 'w')
+			f.close()
 		
-		#Not root directory
-		if len(path.split('/')) != 2:
-			self.files[path[:path.rfind('/')]]['f_dir'].append(path)
-			#Store parentdir metadata
-			pickle.dump(self.files[path[:path.rfind('/')]], open('/cache' + path[:path.rfind('/')] + '.dmeta', 'w+')) 
 		else:
-		#Root directory
-			self.files['/']['f_dir'].append(path)
-			pickle.dump(self.files, open('/cache/root.dmeta', 'w+'))
+			logger.info("Contents of %s brought in from server, %s" % (path, res['Content-x']))
+			#File does not exist, unpickle metadata from string
+			meta = pickle.loads(res['Meta'])
+			#Update in-memory metadata using new information
+                        self.files[path] = meta
 
-		#Store newfile meta
-		pickle.dump(self.files[path], open('/cache' + path + '.meta', 'w+'))
-		f = open('/cache' + path, 'w')
-		f.close()
+                        #Not root directory
+                        if len(path.split('/')) != 2:
+                                self.files[path[:path.rfind('/')]]['f_dir'].append(path)
+                                #Store parentdir metadata
+                                pickle.dump(self.files[path[:path.rfind('/')]], open('/cache' + path[:path.rfind('/')] + '.dmeta', 'w+'))
+                        else:
+                        #Root directory
+                                self.files['/']['f_dir'].append(path)
+                                pickle.dump(self.files, open('/cache/root.dmeta', 'w+'))
 
-        	self.fd += 1
-        	return self.fd
+                        #Store newfile meta
+			f = open('/cache' + path + '.meta', 'w+')
+			f.write(res['Meta'])
+			f.close
+                        f = open('/cache' + path, 'w+')
+                        f.write(res['Content-x'])
+			f.close()
+		
+		return self.fd + 1
 
     	def getattr(self, path, fh = None):
+		logger.info("Getting attributes of file %s", path)
 		"""If file not present, return error"""
         	if path not in self.files:
             		raise FuseOSError(ENOENT)
@@ -96,6 +148,7 @@ class FS(Operations):
 		"""Create a new directory with the default linux flag
 		for a directory, S_IFDIR ORed with any additional modes
 		specified by the application"""
+		logger.info("Creating new directory %s with mode %s" % (path, mode))
         	self.files[path] = dict(st_mode=(S_IFDIR | mode), st_nlink=2,
                                 st_size=0, st_ctime=time(), st_mtime=time(),
                                 st_atime=time(), f_dir = [])
@@ -117,10 +170,12 @@ class FS(Operations):
 		pickle.dump(self.files, open('/cache/root.dmeta', 'w+'))
 
     	def open(self, path, flags):
+		logger.info("Opening file %s with flags %s" % (path, flags))
         	self.fd += 1
         	return self.fd
 
     	def read(self, path, size, offset, fh):
+		logger.info("Reading %d bytes of file %s from offset %d" % (size, path, offset))
         	return self.data[path][offset:offset + size]
 
     	def readdir(self, path, fh):
@@ -128,6 +183,7 @@ class FS(Operations):
 		gotta fix multi-level directories - How does this work?
 		A dir. inside a dir., /hello/dude starts with the prefix /hello/ which
 		only to those of path + 1"""
+		logger.info("Reading directory %s" % path)
         	return ['.', '..'] + [x[x.rfind('/') + 1:] for x in self.files[path]['f_dir']]
 
     	def readlink(self, path):
@@ -157,6 +213,7 @@ class FS(Operations):
 		"""Remove directory by popping old filepath
 		and removing file counter, have to implement
 		recursive removal rm -rf"""
+		logger.info("Removing %s", path)
         	self.files.pop(path)
 
 		if len(old.split('/')) != 2:
@@ -185,7 +242,14 @@ class FS(Operations):
         	self.files[path]['st_size'] = length
 
     	def unlink(self, path):
+		if path not in self.files.keys():
+			raise Exception('File does not exist')
+		logger.info('File %s deleted' % path)
         	self.files.pop(path)
+		self.files['/']['f_dir'].remove(path)
+		os.remove('/cache' + path)
+		os.remove('/cache' + path + '.meta')
+		pickle.dump(self.files, open('/cache/root.dmeta', 'w+'))
 
     	def utimens(self, path, times=None):
         	now = time()
@@ -194,6 +258,7 @@ class FS(Operations):
         	self.files[path]['st_mtime'] = mtime
 
     	def write(self, path, data, offset, fh):
+		logger.info("Writing into file %s from offset %d" % (path, offset))
         	self.data[path] = self.data[path][:offset] + data
         	self.files[path]['st_size'] = len(self.data[path])
 		pickle.dump(self.files[path], open('/cache' + path + '.meta', 'w+'))
@@ -203,9 +268,22 @@ class FS(Operations):
         	return len(data)
 
 
+class Service(rpyc.Service):
+	def exposed_hello(self):
+		logger.info('Server says Hello')
+
+def start_here(t):
+	logger.info('Client RPC Started')
+	t = ThreadedServer(Service, port = 5000)
+	t.start()
+
 if __name__ == '__main__':
     	if len(argv) != 2:
         	print('Usage: %s <mountpoint>' % argv[0])
         	exit(1)
 
+	_config_logging()
+	#start_new_thread(start_here, (None,))
+
     	fuse = FUSE(FS(), argv[1], foreground = True)
+	logger.info('File System mounted at /%s' % argv[1])
